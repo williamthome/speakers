0972e680f5f93b610a559ee7cc927f946f34a475
William Fank Thomé <willegp88@gmail.com>
2021-09-23 15:12:43 -0300

    chore: copy initial files from course repository
    The instuctor make available initial files [here](https://github.com/pkellner/pluralsight-designing-react-components-course-code/tree/master/02-designing-better-components/clip-05-building-functional-components)

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
+import { data } from "../SpeakerData"
+
 function IndexPage() {
+  const { id, bio, company, favorite, first, last, sessions, twitterHandle } = data[0]
+
   return (
-    <div>Hello, World!</div>
+    <div className="container speakers-list">
+      <div className="row">
+        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+          <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
+            <img
+              className="contain-fit"
+              src={`/images/speaker-${id}.jpg`}
+              alt={`${first} ${last}`}
+              width="300"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
