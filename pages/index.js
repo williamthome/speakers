b056228089a35260bd2147f5915586ced22eb4f5
William Fank Thomé <willegp88@gmail.com>
2021-09-23 20:46:25 -0300

    refactor: split speaker card into components

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,89 @@
 import { data } from "../SpeakerData"
 
+function Session({
+  title,
+  room,
+}) {
+  return (
+    <span className="session w-100">
+      {title} <strong>Room: {room.name}</strong>
+    </span>
+  )
+}
+
+function Sessions({ sessions }) {
+  return (
+    <div className="sessionBox card h-250">
+      <Session {...sessions[0]} />
+    </div>
+  )
+}
+
+function SpeakerImage({
+  id,
+  first,
+  last,
+}) {
+  return (
+    <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
+      <img
+        className="contain-fit"
+        src={`/images/speaker-${id}.jpg`}
+        alt={`${first} ${last}`}
+        width="300"
+      />
+    </div>
+  )
+}
+
+function SpeakerDemographics({
+  first,
+  last,
+  bio,
+  company,
+  twitterHandle,
+  favorite,
+}) {
+  return (
+    <div className="speaker-info">
+      <div className="d-flex justify-content-between mb-3">
+        <h3 className="text-truncate w-200">
+          {first} {last}
+        </h3>
+      </div>
+      <div>
+        <p>{bio} {company} {twitterHandle} {favorite}</p>
+      </div>
+    </div>
+  )
+}
+
+function Speaker({
+  speaker
+}) {
+  return (
+    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4" >
+      <div className="card card-height p-4 mt-4">
+        <SpeakerImage {...speaker} />
+        <SpeakerDemographics {...speaker} />
+        <Sessions sessions={speaker.sessions} />
+      </div>
+    </div>
+  )
+}
+
 function IndexPage() {
   return (
     <div className="container speakers-list">
       <div className="row">
-        {data.map(({
-          id,
-          bio,
-          company,
-          favorite,
-          first,
-          last,
-          sessions,
-          twitterHandle,
-        }) =>
-          <div
-            key={id}
-            className="col-xs-12 col-sm-12 col-md-6 col-lg-4"
-          >
-            <div className="card card-height p-4 mt-4">
-              <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
-                <img
-                  className="contain-fit"
-                  src={`/images/speaker-${id}.jpg`}
-                  alt={`${first} ${last}`}
-                  width="300"
-                />
-              </div>
-              <div className="speaker-info">
-                <div className="d-flex justify-content-between mb-3">
-                  <h3 className="text-truncate w-200">
-                    {first} {last}
-                  </h3>
-                </div>
-                <div>
-                  <p>{bio} {company} {twitterHandle} {favorite}</p>
-                </div>
-              </div>
-              <div className="sessionBox card h-250">
-                <span className="session w-100">
-                  {sessions[0].title} <strong>Room: {sessions[0].room.name}</strong>
-                </span>
-              </div>
-            </div>
-          </div>
-        )}
+        {data.map((speaker) => {
+          return (
+            <Speaker
+              key={speaker.id}
+              speaker={speaker}
+            />
+          )
+        })}
       </div>
     </div>
   )
