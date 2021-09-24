1d35ea256d02253aad7f03ff52010c33633222b3
William Fank Thomé <willegp88@gmail.com>
2021-09-24 15:56:30 -0300

    feat: render according to the request status

--- src/components/SpeakersList.js
+++ src/components/SpeakersList.js
@@ -3,14 +3,27 @@ import Speaker from "./Speaker"
 import { data } from "../../SpeakerData"
 import delay from "../utils/delay"
 
-function SpeakersList({ showSessions }) {
+const RequestStatus = Object.freeze({
+  Loading: Symbol("loading"),
+  Success: Symbol("success"),
+  Failure: Symbol("failure"),
+})
 
+function SpeakersList({ showSessions }) {
   const [speakersData, setSpeakersData] = useState([])
+  const [requestStatus, setRequestStatus] = useState(RequestStatus.Loading)
+  const [error, setError] = useState("")
 
   useEffect(() => {
     async function asyncFn() {
-      await delay(2000)
-      setSpeakersData(data)
+      try {
+        await delay(2000)
+        setSpeakersData(data)
+        setRequestStatus(RequestStatus.Success)
+      } catch (reason) {
+        setError(reason)
+        setRequestStatus(RequestStatus.Failure)
+      }
     }
     asyncFn()
   }, [])
@@ -32,24 +45,38 @@ function SpeakersList({ showSessions }) {
     setSpeakersData(updatedData)
   }
 
-  return (
-    <div className="container speakers-list">
-      <div className="row">
-        {speakersData.map((speaker) => {
-          return (
-            <Speaker
-              key={speaker.id}
-              speaker={speaker}
-              showSessions={showSessions}
-              onFavoriteToggle={() =>
-                onFavoriteToggle(speaker.id)
-              }
-            />
-          )
-        })}
+  const render = {
+    [RequestStatus.Loading]: (
+      <div>Loading...</div>
+    ),
+
+    [RequestStatus.Failure]: (
+      <div className="text-danger">
+        ERROR: <b>loading Speaker Data Failed {error}</b>
       </div>
-    </div>
-  )
+    ),
+
+    [RequestStatus.Success]: (
+      <div className="container speakers-list">
+        <div className="row">
+          {speakersData.map((speaker) => {
+            return (
+              <Speaker
+                key={speaker.id}
+                speaker={speaker}
+                showSessions={showSessions}
+                onFavoriteToggle={() =>
+                  onFavoriteToggle(speaker.id)
+                }
+              />
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+
+  return render[requestStatus]
 }
 
 export default SpeakersList
