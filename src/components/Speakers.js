639dab41536253c5db35fdb654652ecf14d705cc
William Fank Thomé <willegp88@gmail.com>
2021-09-24 01:36:52 -0300

    feat: speakers toolbar

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -1,10 +1,12 @@
 import { data } from "../../SpeakerData"
+import SpeakersToolbar from "./SpeakersToolbar"
 import Header from "./Header"
 import SpeakersList from "./SpeakersList"
 
 function Speakers() {
   return (
     <div className="container-fluid">
+      <SpeakersToolbar />
       <Header />
       <SpeakersList data={data} />
     </div>
