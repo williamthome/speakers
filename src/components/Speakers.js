c2caf740b6a374f9969a3ddb0a93b83b9fcca024
William Fank Thomé <willegp88@gmail.com>
2021-09-23 21:04:59 -0300

    refactor: move speakers list render to a component

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -1,19 +1,10 @@
 import { data } from "../../SpeakerData"
-import Speaker from "./Speaker"
+import SpeakersList from "./SpeakersList"
 
 function Speakers() {
   return (
-    <div className="container speakers-list">
-      <div className="row">
-        {data.map((speaker) => {
-          return (
-            <Speaker
-              key={speaker.id}
-              speaker={speaker}
-            />
-          )
-        })}
-      </div>
+    <div className="container-fluid">
+      <SpeakersList data={data} />
     </div>
   )
 }
---
+++ src/components/SpeakersList.js
@@ -0,0 +1,20 @@
+import Speaker from "./Speaker"
+
+function SpeakersList({ data }) {
+  return (
+    <div className="container speakers-list">
+      <div className="row">
+        {data.map((speaker) => {
+          return (
+            <Speaker
+              key={speaker.id}
+              speaker={speaker}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default SpeakersList
