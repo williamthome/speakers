c260363551f697cf0f18d8855a7580e226af97ba
William Fank Thomé <willegp88@gmail.com>
2021-09-23 21:02:12 -0300

    refactor: remove index page responsability to handle data

---
+++ src/components/Speakers.js
@@ -0,0 +1,21 @@
+import { data } from "../../SpeakerData"
+import Speaker from "./Speaker"
+
+function Speakers() {
+  return (
+    <div className="container speakers-list">
+      <div className="row">
+        {data.map((speaker) => {
+          return (
+            <Speaker
+              key={speaker.id}
+              speaker={speaker}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Speakers
