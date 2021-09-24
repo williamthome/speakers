69263466fa8e0414218fcc681caafb5dc6d99f3a
William Fank Thomé <willegp88@gmail.com>
2021-09-24 00:00:33 -0300

    feat: header component

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+function Header() {
+  return (
+    <div className="padT4 padB4">
+      <div className="container mobile-container">
+        <div className="d-flex justify-content-between">
+          <div>
+            <img
+              src="/images/SVCClogo.png"
+              alt="SVCC Home Page"
+            />
+          </div>
+          <div className="light">
+            <h4 className="header-title">
+              Silicon Valley Code Camp
+            </h4>
+          </div>
+          <div className="text-dark">
+            Hello Mr. Smith &nbsp;&nbsp;
+            <span>
+              <a href="#">
+                sign-out
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -1,9 +1,11 @@
 import { data } from "../../SpeakerData"
+import Header from "./Header"
 import SpeakersList from "./SpeakersList"
 
 function Speakers() {
   return (
     <div className="container-fluid">
+      <Header />
       <SpeakersList data={data} />
     </div>
   )
